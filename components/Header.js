c14edaafebe0ebdb4524e34afe110a9ce52b4f02
Vivek K J <vivekkj2004@gmail.com>
2021-10-15 18:49:53 +0530

    Implement Dark Mode 🌛

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,20 @@
 import styles from '../styles/Header.module.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faMoon} from '@fortawesome/free-regular-svg-icons'
+import { faSun} from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
+import {useTheme} from 'next-themes'
 const Header = () => {
+    const {theme, setTheme}= useTheme()
     return (
         <div className={
             styles.Header
         }>
             <Link href='/' passHref={true}>
             <h2>Where In The World?</h2></Link>
-            <h4 className={styles.darkToggle}>
-                <FontAwesomeIcon className={styles.MoonIcon} icon={faMoon}/>
-                Dark Mode
+            <h4 className={styles.darkToggle} onClick={()=> theme==='light'?setTheme('dark'):setTheme('light')}>
+                <FontAwesomeIcon className={styles.MoonIcon} icon={theme==='light'?faMoon:faSun}/>
+                {theme==='light'?"Dark":"Light"} Mode
             </h4>
 
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import {ThemeProvider} from 'next-themes'
 
 function MyApp({Component, pageProps}) {
     return (
-        <Layout>
-            <Component {...pageProps}/>
-        </Layout>
+        <ThemeProvider defaultTheme='light'>
+            <Layout>
+                <Component {...pageProps}/>
+            </Layout>
+        </ThemeProvider>
     )
 }
 
